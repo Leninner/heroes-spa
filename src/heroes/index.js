ca3f630c3dac09b1e18ef29dd1b22b6f40d4e332
leninner <mazabandalenin180@gmail.com>
2022-07-27 13:09:33 -0500

    Initial commit

---
+++ src/heroes/index.js
@@ -0,0 +1,9 @@
+// export * from './components';
+
+// export * from './helpers';
+
+// export * from './hooks';
+
+export * from './pages';
+export * from './routes/HeroesRoutes';
+export * from './data/heroes';
---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,21 @@
+import { Navbar } from '../../ui';
+import { Navigate, Routes, Route } from 'react-router-dom';
+import { DCPage, HeroPage, MarvelPage, SearchPage } from '../pages';
+
+export const HeroesRoutes = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Navigate to='marvel' />} />
+          <Route path='marvel' element={<MarvelPage />} />
+          <Route path='dc' element={<DCPage />} />
+          <Route path='search' element={<SearchPage />} />
+          <Route path='hero' element={<HeroPage />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
