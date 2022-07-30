d1b5773af45c9da97b3091afaba7da8913f4396a
leninner <mazabandalenin180@gmail.com>
2022-07-29 19:34:48 -0500

    Update project

--- src/heroes/components/HeroeList.jsx
+++ src/heroes/components/HeroeList.jsx
@@ -1,13 +1,14 @@
+import { HeroCard } from './HeroCard';
 import { getHeroesByPublisher } from '../helpers';
 
 export const HeroeList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
 
   return (
-    <ul>
+    <div className='row row-cols-3 row-cols-md-3 g-3'>
       {heroes.map((heroe) => (
-        <li key={heroe.id}>{heroe.superhero}</li>
+        <HeroCard key={heroe.id} {...heroe} />
       ))}
-    </ul>
+    </div>
   );
 };
--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,3 +1,18 @@
+import { useParams, Navigate } from 'react-router-dom';
+import { getHeroById } from '../helpers';
+
 export const HeroPage = () => {
-  return <div>HeroPage</div>;
+  const { heroId } = useParams();
+
+  const hero = getHeroById(heroId);
+
+  if (!hero) {
+    return <Navigate to='/marvel' />;
+  }
+
+  return (
+    <div>
+      <h1>{hero.superhero}</h1>
+    </div>
+  );
 };
