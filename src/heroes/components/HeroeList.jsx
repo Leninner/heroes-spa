5768c54232a04aea45bb5a2bcaaaed46e1a5b95a
leninner <mazabandalenin180@gmail.com>
2022-08-01 16:05:40 -0500

    Adding useMemo

--- src/heroes/components/HeroeList.jsx
+++ src/heroes/components/HeroeList.jsx
@@ -1,8 +1,9 @@
 import { HeroCard } from './HeroCard';
 import { getHeroesByPublisher } from '../helpers';
+import { useMemo } from 'react';
 
 export const HeroeList = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
 
   return (
     <div className='row row-cols-3 row-cols-md-3 g-3'>
