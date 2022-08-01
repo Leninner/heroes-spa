d15b7b158a54277ecaf62ee3465cf6c802a38b2d
leninner <mazabandalenin180@gmail.com>
2022-08-01 17:53:32 -0500

    Adding search components

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,26 @@
-import { HeroCard } from '../components';
+import { HeroCard } from '../components'
+import { useForm } from '../../hooks'
+import { useLocation, useNavigate } from 'react-router-dom'
+import queryString from 'query-string'
+import { getHeroesByName } from '../helpers'
 
 export const SearchPage = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const { q = '' } = queryString.parse(location.search)
+
+  const { searchText, onInputChange } = useForm({
+    searchText: q,
+  })
+
+  const heroes = getHeroesByName(q)
+
+  const onSearchSubmit = e => {
+    e.preventDefault()
+
+    navigate(`?q=${searchText.toLowerCase().trim()}`)
+  }
+
   return (
     <>
       <h1>Search</h1>
@@ -11,13 +31,15 @@ export const SearchPage = () => {
           <h4>Searching...</h4>
           <hr />
 
-          <form action=''>
+          <form onSubmit={onSearchSubmit}>
             <input
               type='text'
               placeholder='Search a Hero'
               className='form-control'
               name='searchText'
-              autocomplete='off'
+              autoComplete='off'
+              value={searchText}
+              onChange={onInputChange}
             />
 
             <button className='btn btn-outline-primary mt-1'>Search</button>
@@ -28,14 +50,21 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className='alert alert-primary'>Search a hero</div>
-          <div className='alert alert-danger'>
-            No hero with <b>ABC</b>
-          </div>
-
-          {/* <HeroCard /> */}
+          {q === '' ? (
+            <div className='alert alert-primary'>Search a hero</div>
+          ) : heroes.length ? (
+            <>
+              {heroes.map(hero => (
+                <HeroCard {...hero} key={hero.id} />
+              ))}
+            </>
+          ) : (
+            <div className='alert alert-danger'>
+              No hero with <b>{q}</b>
+            </div>
+          )}
         </div>
       </div>
     </>
-  );
-};
+  )
+}
